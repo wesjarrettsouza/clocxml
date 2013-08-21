853cba0c561fc586c0e273f5e179103e657b6b10
Wes Souza <wsouza@zynga.com>
2013-08-21 11:52:07 -0700

    Added the Apache 2.0 License declaration in every source code as well as a copy of the license at the root of the repository.

--- proj/includes/OCXMLOptions.h
+++ proj/includes/OCXMLOptions.h
@@ -2,7 +2,22 @@
 //  OCXMLOptions.h
 //
 //  Created by Wes Souza.
-//  Copyright (c) 2013 Zynga. All rights reserved.
+//
+//  ------------------------
+//  Copyright 2013 Zynga Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  ------------------------
 //
 //  We declare a class to handle parsing and storing command line options and
 //  arguments. This class also handles generating the options to pass to Clang
